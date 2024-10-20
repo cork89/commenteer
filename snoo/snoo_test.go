e9cb3c1a6d1c5bad66fd269410d9c2ed6e5a2d3b
Sean Larabell <smlarabell@gmail.com>
2024-10-20 16:28:49 -0400

    fix tests with missing env vars

--- snoo/snoo_test.go
+++ snoo/snoo_test.go
@@ -112,13 +112,17 @@ func TestExistingLink_Returned(t *testing.T) {
 }
 
 func TestNewLink_Returned(t *testing.T) {
+	t.Setenv("IMGPROXY_URL", "example.com")
+	t.Setenv("IMGPROXY_SALT", "salt")
+	t.Setenv("IMGPROXY_KEY", "key")
+
 	redditReq := c.RedditRequest{Subreddit: "pics", Article: "1fe0l1d", Comment: "lmlaavt"}
 	userId := 1
 	cmt1 := c.Comment{Comment: "Just here to watch people who voted for the host of “Celebrity Apprentice” (who had never held elected office before running for President) say that nobody should care what a celebrity says.", Author: "MyDesign630"}
 	cmt2 := c.Comment{Comment: "Go read the replies to FuckJerry’s Instagram announcing Swift is voting for Harris. Every reply is “people who care about who celebs vote for are losers”", Author: "Chessh2036"}
 	cmt3 := c.Comment{Comment: "Weird for a group that seems to care a lot about what Kevin Sorbo and Kid Rock thinks, even though I never hear about them anymore otherwise.", Author: "mtaw"}
 	cmts := []c.Comment{cmt1, cmt2, cmt3}
-	want := &c.Link{RedditComments: cmts, ImageUrl: "https://i.redd.it/h2y07ob2m3od1.png", LinkType: c.Image, ProxyUrl: "http://localhost:8080/crYrtsQ3GJQ_H7yXUlf0PrPZmlK7NGBYqT9WkvAF09Q/resize:fit:1024:0:1/padding:0:0/wm:1:soea:0:0:0.5/background:255:255:255/plain/https://i.redd.it/h2y07ob2m3od1.png"}
+	want := &c.Link{RedditComments: cmts, ImageUrl: "https://i.redd.it/h2y07ob2m3od1.png", LinkType: c.Image, ProxyUrl: "http://example.com/EEE_EwE1hWgnFFxUgvBC7Wrd8GqA1yR8Z5lUVjp_YuA/resize:fit:1024:0:1/padding:0:0/wm:1:soea:0:0:0.5/background:255:255:255/plain/https://i.redd.it/h2y07ob2m3od1.png"}
 	user := &c.User{UserId: userId}
 
 	dataaccess.Initialize("local")
@@ -131,13 +135,16 @@ func TestNewLink_Returned(t *testing.T) {
 }
 
 func TestNewLink_SavedToDataAccess(t *testing.T) {
+	t.Setenv("IMGPROXY_URL", "example.com")
+	t.Setenv("IMGPROXY_SALT", "salt")
+	t.Setenv("IMGPROXY_KEY", "key")
 	redditReq := c.RedditRequest{Subreddit: "pics", Article: "1fe0l1d", Comment: "lmlaavt"}
 	userId := 1
 	cmt1 := c.Comment{Comment: "Just here to watch people who voted for the host of “Celebrity Apprentice” (who had never held elected office before running for President) say that nobody should care what a celebrity says.", Author: "MyDesign630"}
 	cmt2 := c.Comment{Comment: "Go read the replies to FuckJerry’s Instagram announcing Swift is voting for Harris. Every reply is “people who care about who celebs vote for are losers”", Author: "Chessh2036"}
 	cmt3 := c.Comment{Comment: "Weird for a group that seems to care a lot about what Kevin Sorbo and Kid Rock thinks, even though I never hear about them anymore otherwise.", Author: "mtaw"}
 	cmts := []c.Comment{cmt1, cmt2, cmt3}
-	want := &c.Link{RedditComments: cmts, ImageUrl: "https://i.redd.it/h2y07ob2m3od1.png", LinkType: c.Image, ProxyUrl: "http://localhost:8080/crYrtsQ3GJQ_H7yXUlf0PrPZmlK7NGBYqT9WkvAF09Q/resize:fit:1024:0:1/padding:0:0/wm:1:soea:0:0:0.5/background:255:255:255/plain/https://i.redd.it/h2y07ob2m3od1.png"}
+	want := &c.Link{RedditComments: cmts, ImageUrl: "https://i.redd.it/h2y07ob2m3od1.png", LinkType: c.Image, ProxyUrl: "http://example.com/EEE_EwE1hWgnFFxUgvBC7Wrd8GqA1yR8Z5lUVjp_YuA/resize:fit:1024:0:1/padding:0:0/wm:1:soea:0:0:0.5/background:255:255:255/plain/https://i.redd.it/h2y07ob2m3od1.png"}
 	user := &c.User{UserId: userId}
 
 	dataaccess.Initialize("local")
